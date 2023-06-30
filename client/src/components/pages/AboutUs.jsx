4f5d1ab9d04712e5dd3f83c57fbcaa3a73838690
moses <echelachris@gmail.com>
2023-06-30 13:10:53 +0100

    removed about dev section

--- client/src/components/pages/AboutUs.jsx
+++ client/src/components/pages/AboutUs.jsx
@@ -13,48 +13,32 @@ const AboutUs = () => {
                     alt="team member"
                 />
                 <div className="text-center md:text-left mr-6">
-                    <h2 className="text-2xl font-medium">About Us</h2>
-                    <p className="text-center text-gray-700 leading-relaxed">
+                    <h2 className="text-1xl font-medium">About Us</h2>
+                    <p className="text-center text-gray-500 leading-relaxed">
                         Our Ethereum sending app is designed to make it easy for users to send and
-                        receive Ethereum tokens. We believe in the power of blockchain
-                        technology and the potential it has to change the world. Our goal is to make it
+                        receive Ethereum tokens. <br />We believe in the power of blockchain
+                        technology and the potential it has to change the world. <br /> Our goal is to make it
                         accessible to everyone, regardless of their technical skill level.
-                    </p>
-                    <p className="text-center text-gray-700 leading-relaxed my-4">
                         Our app is user-friendly and secure, with state-of-the-art encryption to protect
-                        your transactions and assets. We also offer low transaction fees and fast transfer
-                        speeds. We are constantly working to improve our service and offer new features
-                        to our users.
-                    </p>
-                    <p className="text-center text-gray-700 leading-relaxed my-4">
+                        your transactions and assets. We also offer low <br /> transaction fees and fast transfer
+                        speeds. We are constantly working to improve our service <br /> and offer new features
+                        to our users.  <br />
                         Our team of experienced developers and blockchain enthusiasts are committed to
                         providing the best experience for our users. We believe in decentralization and the
                         democratization of finance, and we are dedicated to making that a reality.
-                    </p>
-                    <p className="text-center text-gray-700 leading-relaxed my-4">
                         We welcome your feedback and suggestions for improving our app. Please don't hesitate
                         to contact us with any questions or concerns. We are always here to help.
+                    </p>
+                    <p className="text-center text-gray-700 leading-relaxed my-4">
+                    </p>
+                    <p className="text-center text-gray-700 leading-relaxed my-4">
+                    </p>
+                    <p className="text-center text-gray-700 leading-relaxed my-4">
 
                     </p>
                 </div>
             </div>
-            <section className="bg-orange-400 py-8">
-                <h2 className="text-2xl font-medium text-center mb-4">Meet Our Developer</h2>
-                <div className="md:grid md:grid gap-4">
-                    <div className="p-4">
-                        <img src={dev} alt="Developer 1" className="rounded-full w-32 mx-auto mb-4" />
-                        <h3 className="text-lg font-medium text-center mb-2">ECHELA CHRISTOPHER MOSES</h3>
-                        <p className="text-gray-700 text-center px-8">
-                            An experienced front-end developer with a focus on user experience. He is responsible for 
-                            building the UI and user interactions of this web app.  He is also an entry-level blockchain developer with 
-                          focus on smart contract and security. He is responsible for 
-                            implementing the smart contract functionality and ensuring the security of the Ethereum sending app.
-                            As at the time of developing this app, Echela Moses is a solidity developer intern with Grandida LLC.
-                        </p>
-                    </div>
-                   
-                </div>
-            </section>
+        
 
 
         </>
